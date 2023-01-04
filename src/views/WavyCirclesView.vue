b1032a9e04ca98d0f87e727d754e72f536614588
unknown <some09one@gmail.com>
2023-01-04 18:26:30 +0800

    fix animation delay

--- src/views/WavyCirclesView.vue
+++ src/views/WavyCirclesView.vue
@@ -32,91 +32,91 @@
       left: 0;
       bottom: 0;
       right: 0;
-      animation-delay: 1.3s;
+      animation-delay: 1.5s;
     }
     & span:nth-child(2) {
       top: 10px;
       left: 10px;
       bottom: 10px;
       right: 10px;
-      animation-delay: 1.2s;
+      animation-delay: 1.4s;
     }
     & span:nth-child(3) {
       top: 20px;
       left: 20px;
       bottom: 20px;
       right: 20px;
-      animation-delay: 1.1s;
+      animation-delay: 1.3s;
     }
     & span:nth-child(4) {
       top: 30px;
       left: 30px;
       bottom: 30px;
       right: 30px;
-      animation-delay: 1s;
+      animation-delay: 1.2s;
     }
     & span:nth-child(5) {
       top: 40px;
       left: 40px;
       bottom: 40px;
       right: 40px;
-      animation-delay: 0.9s;
+      animation-delay: 1.1s;
     }
     & span:nth-child(6) {
       top: 50px;
       left: 50px;
       bottom: 50px;
       right: 50px;
-      animation-delay: 0.8s;
+      animation-delay: 1s;
     }
     & span:nth-child(7) {
       top: 60px;
       left: 60px;
       bottom: 60px;
       right: 60px;
-      animation-delay: 0.7s;
+      animation-delay: 0.9s;
     }
     & span:nth-child(8) {
       top: 70px;
       left: 70px;
       bottom: 70px;
       right: 70px;
-      animation-delay: 0.6s;
+      animation-delay: 0.8s;
     }
     & span:nth-child(9) {
       top: 80px;
       left: 80px;
       bottom: 80px;
       right: 80px;
-      animation-delay: 0.5s;
+      animation-delay: 0.7s;
     }
     & span:nth-child(10) {
       top: 90px;
       left: 90px;
       bottom: 90px;
       right: 90px;
-      animation-delay: 0.4s;
+      animation-delay: 0.6s;
     }
     & span:nth-child(11) {
       top: 100px;
       left: 100px;
       bottom: 100px;
       right: 100px;
-      animation-delay: 0.3s;
+      animation-delay: 0.5s;
     }
     & span:nth-child(12) {
       top: 110px;
       left: 110px;
       bottom: 110px;
       right: 110px;
-      animation-delay: 0.2s;
+      animation-delay: 0.4s;
     }
     & span:nth-child(13) {
       top: 120px;
       left: 120px;
       bottom: 120px;
       right: 120px;
-      animation-delay: 0.2s;
+      animation-delay: 0.3s;
     }
     & span:nth-child(14) {
       top: 130px;
