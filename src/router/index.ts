fe6d9a3f1de981a034851aaabff78cda1c3a5e89
unknown <some09one@gmail.com>
2023-01-04 19:17:22 +0800

    add ScrollingText

--- src/router/index.ts
+++ src/router/index.ts
@@ -24,6 +24,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/WavyCirclesView',
     name: 'WavyCirclesView',
     component: () => import('@/views/WavyCirclesView.vue')
+  },
+  {
+    path: '/ScrollingTextView',
+    name: 'ScrollingTextView',
+    component: () => import('@/views/ScrollingTextView.vue')
   }
 ]
 
---
+++ src/views/ScrollingTextView.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="ScrollingTextView">
+    <section>
+      <div>
+        <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum ut accusantium iure sapiente modi, velit minus nihil quae aperiam cupiditate, possimus magni deserunt suscipit dolorum dolores quos doloribus atque ex.</h2>
+      </div>
+      <div>
+        <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum ut accusantium iure sapiente modi, velit minus nihil quae aperiam cupiditate, possimus magni deserunt suscipit dolorum dolores quos doloribus atque ex.</h2>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,800,900&display=swap');
+#ScrollingTextView {
+  font-family: 'Source Sans Pro', sans-serif;
+  & section {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    overflow: hidden;
+    & div {
+      position: relative;
+      width: 50%;
+      background-color: #20506b;
+      overflow: hidden;
+      & h2 {
+        position: absolute;
+        white-space: nowrap;
+        font-size: 12em;
+        line-height: 100vh;
+        color: #fff;
+        animation: animate 60s linear infinite;
+      }
+    }
+
+    & div:nth-child(1) {
+      position: relative;
+      width: 50%;
+      // background-color: url(test.jpg);
+      overflow: hidden;
+      & h2 {
+        -webkit-text-stroke: 2px;
+        -webkit-text-stroke-color: #fff;
+        -webkit-text-fill-color: transparent;
+        left: 100%;
+      }
+    }
+  }
+}
+
+@keyframes animate {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
+}
+</style>
